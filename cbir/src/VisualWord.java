1300c25ac2f8b2b47d5b9629cc3b2477c322a55d
Jonas Schwammberger <hooovercat@gmail.com>
2014-06-01 13:24:20 +0200

    okay

--- cbir/src/VisualWord.java
+++ cbir/src/VisualWord.java
@@ -1,14 +1,14 @@
 import mpi.cbg.fly.Feature;
 
-
 public class VisualWord {
-	
+
 	//the Cebir FeatureVector
 	public Feature centroied;
-	
+
+	public float[] descriptor;
 	//the unique class ID
-	public int	classID;
-	
+	public int classID;
+
 	//a placeholder for a class verification value
-	public Object verificationValue;	
+	public Object verificationValue;
 }
